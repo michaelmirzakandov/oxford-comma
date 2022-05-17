11a23d9e41f298f603d232ac34d91c8273901bfa
Michael <mtrix92@gmail.com>
2022-05-16 20:59:28 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.size == 1 
+        return array[0]
+    end
+    if array.size == 2
+        return "#{array[0]} and #{array[1]}"
+    end
+    if array.size > 2
+        array2 = []
+        counter = 0
+        while counter <= array.length-2
+                array2 << "#{array[counter]}, "
+                counter +=1
+        end
+        array2 <<  "and #{array[counter]}"
+        return array2.join
+    end
 end
